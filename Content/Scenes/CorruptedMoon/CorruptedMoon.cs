88849632ed54509157143cdbc8aae746f4a29e2c
AjuliDev <argon.bright@outlook.com>
2026-04-24 20:17:44 +0300

    fix(CorruptedMoon) added GetWeight override.

--- Content/Scenes/CorruptedMoon/CorruptedMoon.cs
+++ Content/Scenes/CorruptedMoon/CorruptedMoon.cs
@@ -8,6 +8,10 @@ public class CorruptedMoon : ModSceneEffect
 	public override string MapBackground => "TheBrokenScript/Content/Scenes/CorruptedMoon/CorruptedMoonMapBackground";
 	public override ModWaterStyle WaterStyle => ModContent.GetInstance<WaterStyleCorruptedMoon>();
 	public override SceneEffectPriority Priority => SceneEffectPriority.BossMedium;
+	public override float GetWeight(Player player)
+	{
+		return 1.0f;
+	}
 	public override void SpecialVisuals(Player player, bool isActive)
 	{
 		if (isActive)
