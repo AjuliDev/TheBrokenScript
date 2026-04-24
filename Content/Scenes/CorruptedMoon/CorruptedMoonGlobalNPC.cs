8f02e9ec5df698ae3bcae235bcaf2426a38e121a
AjuliDev <argon.bright@outlook.com>
2026-04-24 20:20:01 +0300

    feat(CorruptedMoonGlobalNPC): Added a GlobalNPC class for the Corrupted Moon event to handle spawning during event.

---
+++ Content/Scenes/CorruptedMoon/CorruptedMoonGlobalNPC.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using Terraria;
+using Terraria.ModLoader;
+using TheBrokenScript.Core;
+namespace TheBrokenScript.Content.Scenes.CorruptedMoon;
+public class CorruptedMoonGlobalNPC : GlobalNPC
+{
+	public override void EditSpawnPool(IDictionary<int, float> pool, NPCSpawnInfo spawnInfo)
+	{
+		var worldData = ModState.GetWorldData();
+		if (worldData.MoonData.MoonPhase != ModState.MoonPhase.Normal && !Main.IsItDay())
+		{
+			pool.Clear();
+			//pool[ModContent.NPCType<NPCs.Execute.Execute>()] = 0.05f;
+			//pool[ModContent.NPCType<NPCs.Observe.Observe>()] = 0.05f;
+			pool[ModContent.NPCType<NPCs.SubAnomalyOne.SubAnomalyOne>()] = 0.05f;
+			pool[ModContent.NPCType<NPCs.SiluetR2.SiluetR2>()] = 0.05f;
+			//pool[ModContent.NPCType<NPCs.Obliteration.Obliteration>()] = 0.05f;
+		}
+	}
+	public override void EditSpawnRate(Player player, ref int spawnRate, ref int maxSpawns)
+	{
+		var worldData = ModState.GetWorldData();
+		if (worldData.MoonData.MoonPhase != ModState.MoonPhase.Normal && !Main.IsItDay())
+		{
+			spawnRate = 60;
+			maxSpawns = 2;
+		}
+	}
+}
