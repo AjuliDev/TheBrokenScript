40c77d2cafd3ed61c189da97940c7a4724ee7810
AjuliDev <argon.bright@outlook.com>
2026-04-26 00:35:34 +0300

    feat(Obliteration): Added deactivation during daytime.

--- Content/NPCs/Obliteration/Obliteration.cs
+++ Content/NPCs/Obliteration/Obliteration.cs
@@ -168,5 +168,12 @@ public class Obliteration : ModNPC
 				}
 			}
 		}
+		if (Main.IsItDay() && Main.netMode != NetmodeID.MultiplayerClient)
+		{
+			NPC.life = 0;
+			NPC.HitEffect();
+			NPC.active = false;
+			NetMessage.SendData(MessageID.SyncNPC, -1, -1, null, NPC.whoAmI);
+		}
 	}
 }
