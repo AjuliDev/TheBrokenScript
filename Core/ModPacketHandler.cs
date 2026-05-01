ae10086c8ebbd71381baf0b2aeefe926cc52f1e8
AjuliDev <argon.bright@outlook.com>
2026-05-01 14:57:09 +0300

    feat(ModPacketHandler): Added new entry for Possessor: "SyncPossessorPlayer"

--- Core/ModPacketHandler.cs
+++ Core/ModPacketHandler.cs
@@ -1,11 +1,14 @@
 ﻿using System.IO;
+using Terraria;
 using Terraria.ModLoader;
+using TheBrokenScript.Content.NPCs.Possessor;
 namespace TheBrokenScript.Core;
 public static class ModPacketHandler
 {
 	internal enum PacketType : byte
 	{
 		SyncWorldData,
+		SyncPossessorPlayer,
 	} 
 	public static void Handle(BinaryReader reader, int whoAmI) // Multiplayer packet handling
 	{
@@ -14,6 +17,13 @@ public static class ModPacketHandler
 		{
 			case PacketType.SyncWorldData:
 				ModState.ClientReceiver(reader);
+				break;
+			case PacketType.SyncPossessorPlayer:
+				int targetPlayer = reader.ReadInt32();
+				if (targetPlayer == Main.myPlayer)
+				{
+					Main.LocalPlayer.GetModPlayer<PossessorPlayer>().Possessed = true;
+				}
 				break;
 			default:
 				ModContent.GetInstance<TheBrokenScript>().Logger.WarnFormat("The Broken Script: Unknown packet type: {0}", packet);
