0ec26e1a373f56e8529e0b6c3f8bfc0a87bbb2e0
AjuliDev <argon.bright@outlook.com>
2026-05-01 15:24:28 +0300

    feat(ModTile): Added null tile and texture

---
+++ Content/Tiles/Null/Null.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+namespace TheBrokenScript.Content.Tiles.Null;
+public class Null : ModTile
+{
+	public override void SetStaticDefaults()
+	{
+		Main.tileSolid[Type] = true;
+		Main.tileBlockLight[Type] = true;
+		Main.tileNoAttach[Type] = true;
+		Main.tileFrameImportant[Type] = false;
+		Main.tileCut[Type] = false;
+		Main.tileAxe[Type] = false;
+		Main.tileHammer[Type] = false;
+		Main.tileMergeDirt[Type] = true;
+		MinPick = 50;
+		DustType = DustID.Ash;
+		AddMapEntry(new Color(0, 0, 0));
+	}
+}
