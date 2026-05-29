4d314cd506e0c8a829bb63fabddff3043998b3e2
AjuliDev <argon.bright@outlook.com>
2026-05-30 00:27:52 +0300

    tweak(Null TileType): EmitParticles

--- Content/Tiles/Null/Null.cs
+++ Content/Tiles/Null/Null.cs
@@ -19,4 +19,9 @@ public class Null : ModTile
 		DustType = DustID.Ash;
 		AddMapEntry(new Color(0, 0, 0));
 	}
+	public override void EmitParticles(int i, int j, Tile tile, short tileFrameX, short tileFrameY, Color tileLight, bool visible)
+	{
+		base.EmitParticles(i, j, tile, tileFrameX, tileFrameY, tileLight, visible);
+		Dust.NewDust(new Vector2(i * 16f, j * 16f), 25, 25, ModContent.DustType<NullDust>(), 0.5f, 0.5f, 0, Color.White, 250);
+	}
 }
