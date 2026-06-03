de28dcee056bde1428c98cfcb40ff9847034d299
AjuliDev <argon.bright@outlook.com>
2026-06-03 10:57:19 +0300

    feat(IModEvent): Added Event_KernelPanic

---
+++ Content/Events/Event_KernelPanic.cs
@@ -0,0 +1,22 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using TheBrokenScript.Common.EventHelpers;
+using TheBrokenScript.Core;
+namespace TheBrokenScript.Content.Events;
+public class Event_KernelPanic : IModEvent
+{
+	public bool IsEnabled(ServerConfig config) => config.Event_KernelPanic;
+	public void StartEvent()
+	{
+		if (Main.dedServ)
+		{
+			ModPacket packet = ModContent.GetInstance<TheBrokenScript>().GetPacket();
+			packet.Write((byte)ModPacketHandler.PacketType.KernelPanicCastToClient);
+			packet.Send(-1, -1);
+		} else if (Main.netMode == NetmodeID.SinglePlayer)
+		{
+			KernelPanicModSystem.Enable();
+		}
+	}
+}
