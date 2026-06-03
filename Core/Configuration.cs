9f9d89f0ce8c7c875abfe1722edf8e63f69da34f
AjuliDev <argon.bright@outlook.com>
2026-06-04 01:33:35 +0300

    tweak(Configuration): Added Event_MaliciousGift

--- Core/Configuration.cs
+++ Core/Configuration.cs
@@ -42,5 +42,8 @@ namespace TheBrokenScript.Core
 		[BackgroundColor(169, 40, 212, 255)]
 		[DefaultValue(true)]
 		public bool Event_Faint { get; set; }
+		[BackgroundColor(169, 40, 212, 255)]
+		[DefaultValue(true)]
+		public bool Event_MaliciousGift { get; set; }
 	}
 }
