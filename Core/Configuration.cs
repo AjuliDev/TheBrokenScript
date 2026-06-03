8213f358dcc88d2a3cb0cabfb2d1d2e75d154ca4
AjuliDev <argon.bright@outlook.com>
2026-06-03 10:58:00 +0300

    tweak(Configuration): Added Event_KernelPanic entry

--- Core/Configuration.cs
+++ Core/Configuration.cs
@@ -36,5 +36,8 @@ namespace TheBrokenScript.Core
 		[BackgroundColor(169, 40, 212, 255)]
 		[DefaultValue(true)]
 		public bool Event_Mangle { get; set; }
+		[BackgroundColor(169, 40, 212, 255)]
+		[DefaultValue(true)]
+		public bool Event_KernelPanic { get; set; }
 	}
 }
