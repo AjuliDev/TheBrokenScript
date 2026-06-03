5d31e4922ac0145642207e9992df288a8ac99bc4
AjuliDev <argon.bright@outlook.com>
2026-06-04 01:07:22 +0300

    tweak(Execute): Nerfed pull movement.

--- Content/NPCs/Execute/Execute.cs
+++ Content/NPCs/Execute/Execute.cs
@@ -225,7 +225,10 @@ public class Execute : ModNPC
 		} else
 		{
 			NPC.velocity.X = MathHelper.Lerp(NPC.velocity.X, 0f, 0.1f);
-			closestPlayerInstance.Center = Vector2.Lerp(closestPlayerInstance.Center, NPC.Top, 0.2f);
+			if (Math.Abs(distanceToPlayer) < (16f * 6f))
+			{
+				closestPlayerInstance.Center = Vector2.Lerp(closestPlayerInstance.Center, NPC.Top, 0.2f);
+			}
 		}
 
 		// Torso Bone Placement
