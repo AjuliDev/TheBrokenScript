2fb489bbfc7eefbb8ccaf85a649c3099252a78ab
AjuliDev <argon.bright@outlook.com>
2026-06-04 10:44:21 +0300

    tweak(Configuration): Added Event_ChunkCorruptor

--- Core/Configuration.cs
+++ Core/Configuration.cs
@@ -45,5 +45,8 @@ namespace TheBrokenScript.Core
 		[BackgroundColor(169, 40, 212, 255)]
 		[DefaultValue(true)]
 		public bool Event_MaliciousGift { get; set; }
+		[BackgroundColor(169, 40, 212, 255)]
+		[DefaultValue(true)]
+		public bool Event_ChunkCorruptor { get; set; }
 	}
 }
