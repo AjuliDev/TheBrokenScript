778cea27c832f2784b80e83aa54b975ad6857d29
AjuliDev <argon.bright@outlook.com>
2026-06-05 16:17:14 +0300

    tweak(CorruptedMoonGlobalNPC): Changed spawn pool logic.

--- Content/Scenes/CorruptedMoon/CorruptedMoonGlobalNPC.cs
+++ Content/Scenes/CorruptedMoon/CorruptedMoonGlobalNPC.cs
@@ -7,10 +7,23 @@ public class CorruptedMoonGlobalNPC : GlobalNPC
 {
 	public override void EditSpawnPool(IDictionary<int, float> pool, NPCSpawnInfo spawnInfo)
 	{
+		var tsbTypes = new HashSet<int>
+		{
+			ModContent.NPCType<NPCs.Execute.Execute>(),
+			ModContent.NPCType<NPCs.Observe.Observe>(),
+			ModContent.NPCType<NPCs.SubAnomalyOne.SubAnomalyOne>(),
+			ModContent.NPCType<NPCs.SiluetR2.SiluetR2>(),
+			ModContent.NPCType<NPCs.Obliteration.Obliteration>(),
+			ModContent.NPCType<NPCs.Follow.Follow>(),
+			ModContent.NPCType<NPCs.Possessor.Possessor>(),
+		};
 		var worldData = ModState.GetWorldData();
 		if (worldData.MoonData.MoonPhase != ModState.MoonPhase.Normal && !Main.IsItDay())
 		{
-			pool.Clear();
+			if (Main.invasionType == 0)
+			{
+				pool.Clear();
+			}
 			pool[ModContent.NPCType<NPCs.Execute.Execute>()] = 0.05f;
 			pool[ModContent.NPCType<NPCs.Observe.Observe>()] = 0.025f;
 			pool[ModContent.NPCType<NPCs.SubAnomalyOne.SubAnomalyOne>()] = 0.10f;
@@ -18,6 +31,15 @@ public class CorruptedMoonGlobalNPC : GlobalNPC
 			pool[ModContent.NPCType<NPCs.Obliteration.Obliteration>()] = 0.025f;
 			pool[ModContent.NPCType<NPCs.Follow.Follow>()] = 0.05f;
 			pool[ModContent.NPCType<NPCs.Possessor.Possessor>()] = 0.05f;
+
+			if (Main.invasionType == 0)
+			{
+				foreach (var key in new List<int>(pool.Keys))
+				{
+					if (!tsbTypes.Contains(key))
+						pool.Remove(key);
+				}
+			}
 		}
 		if (worldData.SunData.SunPhase != ModState.SunPhase.Normal && Main.IsItDay())
 		{
@@ -25,6 +47,14 @@ public class CorruptedMoonGlobalNPC : GlobalNPC
 			pool[ModContent.NPCType<NPCs.SubAnomalyOne.SubAnomalyOne>()] = 0.075f;
 			pool[ModContent.NPCType<NPCs.Observe.Observe>()] = 0.075f;
 			pool[ModContent.NPCType<NPCs.Execute.Execute>()] = 0.075f;
+			if (Main.invasionType == 0)
+			{
+				foreach (var key in new List<int>(pool.Keys))
+				{
+					if (!tsbTypes.Contains(key))
+						pool.Remove(key);
+				}
+			}
 		}
 	}
 	public override void EditSpawnRate(Player player, ref int spawnRate, ref int maxSpawns)
@@ -32,8 +62,13 @@ public class CorruptedMoonGlobalNPC : GlobalNPC
 		var worldData = ModState.GetWorldData();
 		if (worldData.MoonData.MoonPhase != ModState.MoonPhase.Normal && !Main.IsItDay()) // Checking light levels does not work that easily for this
 		{
-			spawnRate = 1200;
-			maxSpawns = 5;
+			spawnRate = 600; //600
+			maxSpawns = 15; // 15
 		}
+		//if (worldData.MoonData.MoonPhase == ModState.MoonPhase.CorruptedRandom && !Main.IsItDay()) // TODO: Add some functionality around CorruptedRandom
+		//{
+		//	spawnRate = 0;
+		//	maxSpawns = 0;
+		//}
 	}
 }
