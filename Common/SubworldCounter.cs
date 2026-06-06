bee4cade1c88824bad86afbc80722b24cc7121c0
AjuliDev <argon.bright@outlook.com>
2026-06-07 01:57:56 +0300

    feat(ModSystem): SubworldCounter for preventing dedServ crashes on multiple simultaneous joins.

---
+++ Common/SubworldCounter.cs
@@ -0,0 +1,19 @@
+﻿using System.Threading.Tasks;
+using Terraria;
+using Terraria.ModLoader;
+
+namespace TheBrokenScript.Common;
+public class SubworldCounter : ModSystem
+{
+	public static int NowhereEntryCooldown = 0;
+	public override void PostUpdateTime()
+	{
+		if (Main.dedServ)
+		{
+			if (NowhereEntryCooldown > 0)
+			{
+				Task.Delay(1000 * NowhereEntryCooldown).ContinueWith(task => NowhereEntryCooldown = 0);
+			}
+		}
+	}
+}
