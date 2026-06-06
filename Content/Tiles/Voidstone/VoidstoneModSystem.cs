b3278801fe129956bcc7d136b6675c16ff2c30e6
AjuliDev <argon.bright@outlook.com>
2026-06-07 02:02:44 +0300

    feat(ModSystem): Added VoidstoneModSystem for Nowhere subworld biome and daytime.

---
+++ Content/Tiles/Voidstone/VoidstoneModSystem.cs
@@ -0,0 +1,30 @@
+﻿using SubworldLibrary;
+using System;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using TheBrokenScript.Content.Subworlds.Nowhere;
+
+namespace TheBrokenScript.Content.Tiles.Voidstone;
+
+public class VoidstoneModSystem : ModSystem
+{
+	public int blockCount;
+	public override void TileCountsAvailable(ReadOnlySpan<int> tileCounts)
+	{
+		blockCount = tileCounts[ModContent.TileType<Voidstone>()];
+	}
+
+	public override void PostUpdateWorld()
+	{
+		if (SubworldSystem.IsActive<Nowhere>())
+		{
+			Main.dayTime = false;
+			Main.time = 13000;
+			if (Main.dedServ)
+			{
+				NetMessage.SendData(MessageID.WorldData);
+			}
+		}
+	}
+}
