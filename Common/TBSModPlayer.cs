3e93830cffb23c1229295b6dd0e46bd9be1feaea
AjuliDev <argon.bright@outlook.com>
2026-06-06 13:16:12 +0300

    feat(ModPlayer): Added TBSModPlayer for custom hit sounds from SiluetR2 basic attacks.

---
+++ Common/TBSModPlayer.cs
@@ -0,0 +1,16 @@
+﻿using Terraria;
+using Terraria.ModLoader;
+using TheBrokenScript.Content.NPCs.SiluetR2;
+using TheBrokenScript.Core;
+namespace TheBrokenScript.Common;
+public class TBSModPlayer : ModPlayer
+{
+	public override void ModifyHitByNPC(NPC npc, ref Player.HurtModifiers modifiers)
+	{
+		base.ModifyHitByNPC(npc, ref modifiers);
+		if (npc.type == ModContent.NPCType<SiluetR2>())
+		{
+			ModSounds.PlaySound("SiluetAttackSimple", 0.4f, (int)Main.LocalPlayer.Center.X, (int)Main.LocalPlayer.Center.Y, 0.4f);
+		}
+	}
+}
