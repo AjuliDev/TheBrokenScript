6e24ceb1a1676ca003b664d64162a35596cadd6c
AjuliDev <argon.bright@outlook.com>
2026-06-06 17:53:23 +0300

    tweak(CorruptedMoonGlobalNPC): Added cave noise on entity spawn and editable max spawns.

--- Content/Scenes/CorruptedMoon/CorruptedMoonGlobalNPC.cs
+++ Content/Scenes/CorruptedMoon/CorruptedMoonGlobalNPC.cs
@@ -1,13 +1,14 @@
 ﻿using System.Collections.Generic;
 using Terraria;
+using Terraria.DataStructures;
+using Terraria.ID;
 using Terraria.ModLoader;
+using Terraria.ModLoader.Config;
 using TheBrokenScript.Core;
 namespace TheBrokenScript.Content.Scenes.CorruptedMoon;
 public class CorruptedMoonGlobalNPC : GlobalNPC
 {
-	public override void EditSpawnPool(IDictionary<int, float> pool, NPCSpawnInfo spawnInfo)
-	{
-		var tsbTypes = new HashSet<int>
+	public static HashSet<int> TBSTypes = new HashSet<int>
 		{
 			ModContent.NPCType<NPCs.Execute.Execute>(),
 			ModContent.NPCType<NPCs.Observe.Observe>(),
@@ -17,6 +18,8 @@ public class CorruptedMoonGlobalNPC : GlobalNPC
 			ModContent.NPCType<NPCs.Follow.Follow>(),
 			ModContent.NPCType<NPCs.Possessor.Possessor>(),
 		};
+	public override void EditSpawnPool(IDictionary<int, float> pool, NPCSpawnInfo spawnInfo)
+	{
 		var worldData = ModState.GetWorldData();
 		if (worldData.MoonData.MoonPhase != ModState.MoonPhase.Normal && !Main.IsItDay())
 		{
@@ -36,7 +39,7 @@ public class CorruptedMoonGlobalNPC : GlobalNPC
 			{
 				foreach (var key in new List<int>(pool.Keys))
 				{
-					if (!tsbTypes.Contains(key))
+					if (!TBSTypes.Contains(key))
 						pool.Remove(key);
 				}
 			}
@@ -51,7 +54,7 @@ public class CorruptedMoonGlobalNPC : GlobalNPC
 			{
 				foreach (var key in new List<int>(pool.Keys))
 				{
-					if (!tsbTypes.Contains(key))
+					if (!TBSTypes.Contains(key))
 						pool.Remove(key);
 				}
 			}
@@ -60,15 +63,44 @@ public class CorruptedMoonGlobalNPC : GlobalNPC
 	public override void EditSpawnRate(Player player, ref int spawnRate, ref int maxSpawns)
 	{
 		var worldData = ModState.GetWorldData();
+		var config = ServerConfig.Instance;
 		if (worldData.MoonData.MoonPhase != ModState.MoonPhase.Normal && !Main.IsItDay()) // Checking light levels does not work that easily for this
 		{
-			spawnRate = 600; //600
-			maxSpawns = 15; // 15
+			spawnRate = 1600; //600
+			if (config == null)
+			{
+				maxSpawns = 10;
+			}
+			else
+			{
+				maxSpawns = config.MaximumEntitiesAllowed;
+			}
 		}
 		//if (worldData.MoonData.MoonPhase == ModState.MoonPhase.CorruptedRandom && !Main.IsItDay()) // TODO: Add some functionality around CorruptedRandom
 		//{
 		//	spawnRate = 0;
 		//	maxSpawns = 0;
 		//}
+	}
+
+	public override void OnSpawn(NPC npc, IEntitySource source)
+	{
+		base.OnSpawn(npc, source);
+
+		if (TBSTypes.Contains(npc.type))
+		{
+			if (Main.dedServ)
+			{
+				ModPacket packet = ModContent.GetInstance<TheBrokenScript>().GetPacket();
+				packet.Write((byte)ModPacketHandler.PacketType.RequestCaveSound);
+				packet.Write((int)(npc.Center.X / 16f));
+				packet.Write((int)(npc.Center.Y / 16f));
+				packet.Send(-1, -1);
+			}
+			else if (Main.netMode == NetmodeID.SinglePlayer)
+			{
+				ModSounds.PlayCaveNoise((int)(npc.Center.X / 16f), (int)(npc.Center.Y / 16f));
+			}
+		}
 	}
 }
