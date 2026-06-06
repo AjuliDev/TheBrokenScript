4429b5b9903dcfa9024ce27f6423e2d5546c55be
AjuliDev <argon.bright@outlook.com>
2026-06-06 17:53:50 +0300

    feat(ModItem): Added Red Obsidian

---
+++ Content/Tiles/RedObsidian/RedObsidianItem.cs
@@ -0,0 +1,15 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace TheBrokenScript.Content.Tiles.RedObsidian;
+
+public class RedObsidianItem : ModItem
+{
+	public override void SetDefaults()
+	{
+		Item.DefaultToPlaceableTile(ModContent.TileType<Tiles.RedObsidian.RedObsidian>(), 0);
+		Item.value = Terraria.Item.sellPrice(0, 0, 0, 25);
+		Item.rare = ItemRarityID.Red;
+	}
+}
