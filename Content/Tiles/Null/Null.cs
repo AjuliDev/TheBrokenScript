615873a4656162ab0b4bc955da0500dbc815c6b0
AjuliDev <argon.bright@outlook.com>
2026-06-06 18:48:42 +0300

    tweak(NullTile): Added config flag for ambient particles.

--- Content/Tiles/Null/Null.cs
+++ Content/Tiles/Null/Null.cs
@@ -2,6 +2,7 @@
 using Terraria;
 using Terraria.ID;
 using Terraria.ModLoader;
+using TheBrokenScript.Core;
 namespace TheBrokenScript.Content.Tiles.Null;
 public class Null : ModTile
 {
@@ -22,9 +23,20 @@ public class Null : ModTile
 	public override void EmitParticles(int i, int j, Tile tile, short tileFrameX, short tileFrameY, Color tileLight, bool visible)
 	{
 		//base.EmitParticles(i, j, tile, tileFrameX, tileFrameY, tileLight, visible);
-		if (Main.rand.NextBool(3))
+		var config = ClientConfig.Instance;
+		if (config != null)
 		{
-			Dust.NewDust(new Vector2(i * 16f, j * 16f), 25, 25, ModContent.DustType<NullDust>(), 0.5f, 0.5f, 0, Color.White, 250);
+			if (Main.rand.NextBool(3) && config.TileAmbientParticles)
+			{
+				Dust.NewDust(new Vector2(i * 16f, j * 16f), 25, 25, ModContent.DustType<NullDust>(), 0.5f, 0.5f, 0, Color.White, 250);
+			}
+		}
+		else
+		{
+			if (Main.rand.NextBool(3))
+			{
+				Dust.NewDust(new Vector2(i * 16f, j * 16f), 25, 25, ModContent.DustType<NullDust>(), 0.5f, 0.5f, 0, Color.White, 250);
+			}
 		}
 	}
 }
