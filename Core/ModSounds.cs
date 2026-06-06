463667167889caef78cab5ae8026a3f10b7202f6
AjuliDev <argon.bright@outlook.com>
2026-06-06 13:19:23 +0300

    feat(ModSounds): Added PlaySound function for easy looping.

--- Core/ModSounds.cs
+++ Core/ModSounds.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using Terraria;
 using Terraria.DataStructures;
@@ -40,5 +41,39 @@ public class ModSounds : ModSystem
 			}
 		}
 		Task.Delay(10000).ContinueWith(task => ActiveSounds.Remove($"CaveNoise{randEntry}"));
+	}
+
+	public static void PlaySound(string sound, float soundTimerSeconds = 1f, int i = 0, int j = 0, float pitchVariance = 0f)
+	{
+		if (ActiveSounds.Contains(sound))
+		{
+			return;
+		}
+		else
+		{
+			ActiveSounds.Add(sound);
+		}
+		float newPitch = Main.rand.NextFloat(0, 1) * pitchVariance;
+		newPitch = Math.Clamp(newPitch, 0f, 1f);
+		if (i == 0 && j == 0)
+		{
+			ModAssets.Sounds[sound].Value.Play(volume: 1f, newPitch, pan: 0f);
+		}
+		else
+		{
+			if (i > (Main.LocalPlayer.position.X / 16) + 20)
+			{
+				ModAssets.Sounds[sound].Value.Play(volume: 1f, newPitch, pan: 0.5f);
+			}
+			else if (i < (Main.LocalPlayer.position.X / 16) - 20)
+			{
+				ModAssets.Sounds[sound].Value.Play(volume: 1f, newPitch, pan: -0.5f);
+			}
+			else
+			{
+				ModAssets.Sounds[sound].Value.Play(volume: 1f, newPitch, pan: 0f);
+			}
+		}
+		Task.Delay((int)(1000f * soundTimerSeconds)).ContinueWith(task => ActiveSounds.Remove(sound));
 	}
 }
